7c5ba9edfd88807af2718ec4a2155529c496ee0a
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2023-09-09 11:43:23 +0700

    Create WingCommand.php

---
+++ src/NurAzliYT/WingsAnimated/command/WingCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace phuongaz\EasyWing\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender
+use pocketmine\player\Player;
+use phuongaz\EasyWing\form\WingsForm;
+
+Class WingsCommand extends Command{
+
+	public function __construct(){
+		parent::__construct("wings", "open wings form");
+	}
+
+	public function execute(CommandSender $sender, string $label, array $args) :bool{
+		if(!$sender instanceof Player) return false;
+		$form = new WingsForm($sender);
+		$form->send();
+		return true;
+	}
+}
